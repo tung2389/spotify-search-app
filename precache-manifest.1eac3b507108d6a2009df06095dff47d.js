3ca010ec32e237576b14b338c44798f212aadf28
tung2389 <36613569+tung2389@users.noreply.github.com>
2019-06-18 10:21:05 +0700

    Add files via upload

---
+++ precache-manifest.1eac3b507108d6a2009df06095dff47d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "4a085593ed514c5c4ac3",
+    "url": "./static/js/main.d3611c74.chunk.js"
+  },
+  {
+    "revision": "5686cbe4d143d536cdce",
+    "url": "./static/js/2.70d59123.chunk.js"
+  },
+  {
+    "revision": "4a085593ed514c5c4ac3",
+    "url": "./static/css/main.19df138b.chunk.css"
+  },
+  {
+    "revision": "a807b1f7724b0418da5b28ab1706959d",
+    "url": "./index.html"
+  }
+];
